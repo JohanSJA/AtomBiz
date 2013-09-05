1bf9202d5dd5f9eb6491575de6ddfa3ed06082af
Johan Sim Jian An <johansja@gmail.com>
2013-09-05 21:35:02 +0800

    More accounts public views.
    - SectionIndex
    - GroupIndex
    - MasterIndex

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('.views',
-    url(r'^$', login_required(TemplateView.as_view(template_name='accounts/index.html')))
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^$', login_required(TemplateView.as_view(template_name='accounts/index.html'))),
+    url(r'^sections/$', login_required(views.SectionIndexView.as_view())),
+    url(r'^groups/$', login_required(views.GroupIndexView.as_view())),
+    url(r'^masters/$', login_required(views.MasterIndexView.as_view()))
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,26 @@
-# Create your views here.
+from django.views import generic
+
+from .models import *
+
+class SectionIndexView(generic.ListView):
+    template_name = 'accounts/sections/index.html'
+    context_object_name = 'section_list'
+    
+    def get_queryset(self):
+        return Section.objects.all()
+
+
+class GroupIndexView(generic.ListView):
+    template_name = 'accounts/groups/index.html'
+    context_object_name = 'group_list'
+    
+    def get_queryset(self):
+        return Group.objects.all()
+    
+
+class MasterIndexView(generic.ListView):
+    template_name = 'accounts/masters/index.html'
+    context_object_name = 'master_list'
+    
+    def get_queryset(self):
+        return Master.objects.all()
