dd1d467290edecf91e118af36f867aba12dfaf3b
Johan Sim Jian An <johansja@gmail.com>
2013-09-14 09:22:05 +0800

    Stock category CRUD.

--- stocks/urls.py
+++ stocks/urls.py
@@ -6,9 +6,10 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', login_required(TemplateView.as_view(template_name='stocks/index.html'))),
-)
-
-urlpatterns += patterns('views',
-    url(r'^masters/$', login_required(views.MasterList.as_view()), name='stocks_master_list'),
-    url(r'^masters/new', login_required(views.MasterCreate.as_view()), name='stocks_master_new')
+    url(r'^category$', login_required(views.CategoryList.as_view()), name='stocks_category_list'),
+    url(r'^category/new$', login_required(views.CategoryCreate.as_view()), name='stocks_category_new'),
+    url(r'^category/(?P<pk>\d+)/edit', login_required(views.CategoryUpdate.as_view()), name='stocks_category_edit'),
+    url(r'^category/(?P<pk>\d+)/delete', login_required(views.CategoryDelete.as_view()), name='stocks_category_delete'),
+    url(r'^masters$', login_required(views.MasterList.as_view()), name='stocks_master_list'),
+    url(r'^masters/new$', login_required(views.MasterCreate.as_view()), name='stocks_master_new')
 )
--- stocks/views.py
+++ stocks/views.py
@@ -1,7 +1,27 @@
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 from .models import *
+
+
+class CategoryList(ListView):
+    model = Category
+
+
+class CategoryCreate(CreateView):
+    model = Category
+    success_url = reverse_lazy('stocks_category_list')
+
+
+class CategoryUpdate(UpdateView):
+    model = Category
+    success_url = reverse_lazy('stocks_category_list')
+
+
+class CategoryDelete(DeleteView):
+    model = Category
+    success_url = reverse_lazy('stocks_category_list')
 
 
 class MasterList(ListView):
